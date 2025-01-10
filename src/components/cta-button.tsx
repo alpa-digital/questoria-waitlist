bf0e097652e745c5600cc72c807734198e1aadc2
Francisco Alé Palacios <fraalepal@alum.us.es>
2025-01-10 12:22:37 +0100

    Update cta-button.tsx

--- src/components/cta-button.tsx
+++ src/components/cta-button.tsx
@@ -9,7 +9,7 @@ export function CTAButton() {
   return (
     <div className="button-wrapper">
       <button
-        onClick={() => router.push('https://contate.me/questoria')}
+        onClick={() => router.push('/')}
         className="w-[calc(100%-2px)] h-[calc(100%-2px)] absolute z-30 top-[1px] left-[1px] text-sm bg-black flex items-center justify-center gap-5 rounded-3xl focus:outline-none"
       >
         Join Waitlist
